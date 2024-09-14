88b520d7f4fc434996b451a53c6e61bc97694009
Shalini Jha <81086975+shalinijha7@users.noreply.github.com>
2024-09-14 09:48:00 +0000

    arithmetic1

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,4 +24,16 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
+//console.log(stringNumber)
+
+//-----------------------------***---------------------***----------
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+let str1 = "hello"
+let str2 = " shalini"
+
+let str3 = str1+ str2
+console.log(str3)
