702959f983a1481cdf04bb2eb05dd5dd94d11be8
Shalini Jha <81086975+shalinijha7@users.noreply.github.com>
2024-09-14 07:40:16 +0000

    container config2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=14455
+let accountEmail="shalini@apple.com"
+var accountPassword="123"
+accountCity="Spain"
+let accountState;
+//accountId=2
+
+accountEmail="shalini@barclays.com"
+accountPassword="212"
+accountCity="Bengaluru"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
