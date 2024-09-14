d20fd8ca32354eb1ed13f2e523548e67ad85bb34
Shalini Jha <81086975+shalinijha7@users.noreply.github.com>
2024-09-14 09:39:12 +0000

    converse data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score="35abc"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+//console.log(valueInNumber) => for score="35" output will be number only
+
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 ; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 =>false
+// "" => false
+// "shalini" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
