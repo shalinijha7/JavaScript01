bca4647731a6285f5ccf98785882a58fcb9308dc
Shalini Jha <81086975+shalinijha7@users.noreply.github.com>
2024-09-14 09:00:52 +0000

    container config3

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) we are using nodejs,not browser so its not going to get executed
+
+console.log(3+3) //code readability whould be high
+console.log("shalini")
+
+let age = 18
+let loggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigTnit
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => used for unique value
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
