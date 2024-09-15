e71aae05df503c19e6f443895f74b073df2c9450
Shalini Jha <81086975+shalinijha7@users.noreply.github.com>
2024-09-15 11:06:51 +0000

    dataTypes1

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,23 @@
+//Primitive
+
+//7 types:string,number,boolean,null,undefined,symbol,bigInt
+
+
+//Reference (Non Primitive) inko memory mai directly allocate kiya jaa sakta hai 
+//Includes array,objects,functions
+
+//1.)Array
+const heros=["shaktiman","naagraj","doga"]
+
+//2.)Object
+let myObj={
+    name:"shalini",
+    age:"24"
+}
+
+//3.)Function
+//->in js function is treated as a variable
+
+const myFunction=function(){
+    console.log("Hello world")
+}
