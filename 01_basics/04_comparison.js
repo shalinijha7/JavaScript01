bea626aebdced54712ab38c576bbf6f40a92daf4
Shalini Jha <81086975+shalinijha7@users.noreply.github.com>
2024-09-15 09:44:30 +0000

    comparsion_operator

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>1)
+
+//we can easily compare same datatype elements with eachother
+//problem occurs when we try to compare two different datatypes 
+
+//here we are comparing string and integer
+// console.log("2" > 1) //true as output
+// console.log("02" > 1) //true as output
+
+// console.log(null>0) //false
+// console.log(null==0) //false
+// console.log(null>=0) //true
+
+// console.log(undefined==0) //false
+// console.log(undefined>0) //false
+// console.log(undefined<0) //false
+
