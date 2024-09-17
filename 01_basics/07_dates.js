79e834dc5b525e5daa5cf1952b5215a9cdbf965a
Shalini Jha <81086975+shalinijha7@users.noreply.github.com>
2024-09-17 14:32:49 +0000

    dates2

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,9 +1,23 @@
 let myDate = new Date()
-console.log(myDate)
+//console.log(myDate)
 // console.log(myDate.toString())
 // console.log(myDate.toDateString())
 // console.log(myDate.toISOString())
 // console.log(myDate.toJSON())
 // console.log(myDate.toLocaleString())
 // console.log(myDate.toLocaleDateString())
-console.log(typeof myDate)
+
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2024,0,24)
+// let myCreatedDate = new Date(2024,0,24,5,3)
+
+let myCreatedDate = new Date("01-14-2024")
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now() //will give total milliseconds of time from a certain level
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+
+console.log(Math.floor(Date.now()/1000))
+
