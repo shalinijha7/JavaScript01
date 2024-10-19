345cfb0090dd15ecd1572919b1b3cf5f1d990c85
Shalini Jha <81086975+shalinijha7@users.noreply.github.com>
2024-10-19 17:54:35 +0000

    promises1

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -21,5 +21,26 @@ new Promise(function(resolve, reject){
 })
 
 const promiseThree = new Promise(function(resolve, reject){
-    
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"})
+    }, 1000)
 })
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "shalini", password: "123"})
+        }else{
+            reject('ERROR: Something went wrong')
+        }
+    },1000)
+})
+
+promiseFour.then((user) => {
+    
+}).catch()
