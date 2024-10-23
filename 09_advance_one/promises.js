abc9cd5a041ba10b1afe69a97c2b610f2b179a2e
Shalini Jha <shalini16jha@gmail.com>
2024-10-23 11:34:06 +0530

    promises2

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -42,5 +42,57 @@ const promiseFour = new Promise(function(resolve, reject){
 })
 
 promiseFour.then((user) => {
+    console.log(user)
+    return user.username
+}).then((username) => {
+    console.log(username)
+}).catch(function(error){
+    console.log(error)
+}).finally(() => {
+    console.log("The promise is either resolved or rejected");
+})
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "javascript", password: "123"})
+        }else{
+            reject('ERROR: JS went wrong')
+        }
+    },1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data)
+//     } catch (error) {
+//         console.log("E: ", error)
+//     }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
     
-}).catch()
+})
+.catch((error) => console.log(error))
