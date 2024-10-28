bd26d73f4a59b1f3eefca406a5563799d3fa4491
Shalini Jha <shalini16jha@gmail.com>
2024-10-28 20:27:54 +0530

    object

---
+++ 10_classes_and_oop/Object.js
@@ -0,0 +1,27 @@
+function multiplyBy5(num){
+    return num*5
+}
+multiplyBy5.power = 2
+
+console.log(multiplyBy5(5))
+console.log(multiplyBy5.power)
+console.log(multiplyBy5.prototype)
+
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+createUser.prototype.printMe = function(){
+    console.log(`Score is ${this.score}`)
+}
+
+const chai = createUser("chai", 25)
+const tea = createUser("tea", 250)
+
+chai.printMe()
+
+/* The new operator lets developers create an instance of a user-defined object type or of one of the built-in object types that has a constructor function.*/
