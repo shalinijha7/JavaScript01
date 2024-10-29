d1a7f2e67aa8957a6cb21accf322ca25d2595d13
Shalini Jha <shalini16jha@gmail.com>
2024-10-29 07:50:21 +0530

    staticProperty

---
+++ 10_classes_and_oop/staticprop.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`Username: ${this.username}`)
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const shalini = new User("shalini")
+console.log(shalini.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId())
